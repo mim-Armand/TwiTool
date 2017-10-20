7cbdda410be33742d33b8b4850c50bec6795f9d8
mim Armand <mim3dot@gmail.com>
2017-10-20 15:17:52 -0500

    started on textInput comp and some other UI stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 //@flow
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TextInput from './components/TextInput/TextInput'
 
 type Props = {
     foo: number,
@@ -14,13 +14,14 @@ class App extends Component<Props, State> {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <TextInput/>
+        {/*<header className="App-header">*/}
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/*<h1 className="App-title">Welcome to React</h1>*/}
+        {/*</header>*/}
+        {/*<p className="App-intro">*/}
+          {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
       </div>
     );
   }
---
+++ client/src/components/TextInput/TextInput.js
@@ -0,0 +1,33 @@
+//@flow
+import React, { Component } from 'react';
+import './TextInput.css';
+
+type Props = {
+    foo: number,
+};
+type State = {
+    bar: number,
+};
+
+class TextInput extends Component<Props, State> {
+  render() {
+    return (
+      <div className="App">
+
+          <span className="input input--nao">
+                <input className="input__field input__field--nao" type="text" id="input-1" />
+                <label className="input__label input__label--nao" htmlFor="input-1">
+                    <span className="input__label-content input__label-content--nao">Username</span>
+                </label>
+                <svg className="graphic graphic--nao" width="300%" height="100%" viewBox="0 0 1200 60" preserveAspectRatio="none">
+                    <path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"/>
+                </svg>
+            </span>
+
+
+      </div>
+    );
+  }
+}
+
+export default TextInput;
