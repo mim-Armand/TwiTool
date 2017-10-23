781f570d9f140d94da3c7da69f6bad59ad7ac417
mim Armand <mim3dot@gmail.com>
2017-10-22 23:36:05 -0500

    More work on UI and features, including window btns like a minimize/maximize btn and fullscreen mode, Please note that font-awesome files are not included int these commits for now so it should be added to the public folder for icons to work, I'll add them once cleaned up in future commits

--- client/src/reducers/initialState.js
+++ client/src/reducers/initialState.js
@@ -1,3 +1,11 @@
 export default {
-    stuff: []
+    stuff: [],
+    test: "test",
+    handle: "mim_Armand",
+    twitter_app:{
+        consumer_key: null,
+        consumer_secret: null,
+        access_token_key: null,
+        access_token_secret: null
+    }
 };
